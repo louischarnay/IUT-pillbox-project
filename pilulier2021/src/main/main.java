dece7cea01e4564a6db7b63fb0b9208128fd79e7
CHARNAY LOUIS p2018172 <louis.charnay@etu.univ-lyon1.fr>
2021-06-15 20:54:01 +0200

    corrections

--- pilulier2021/src/main/main.java
+++ pilulier2021/src/main/main.java
@@ -9,6 +9,7 @@ import com.pi4j.io.gpio.RaspiBcmPin;
 import com.pi4j.device.pibrella.PibrellaBuzzer;
 import main.vue.Interface;
 import ss2_rpi_2021.DigitaBCMGpio;
+import ss2_rpi_2021.StepperMotorGpio;
 
 /**
  *
@@ -23,14 +24,17 @@ public class main {
         fenetre.setVisible(true);
         // TODO code application logic here
         
-        DigitaBCMGpio digitaBCMGpio = new DigitaBCMGpio(RaspiBcmPin.GPIO_26);
-        digitaBCMGpio.start();
+//        DigitaBCMGpio digitaBCMGpio = new DigitaBCMGpio(RaspiBcmPin.GPIO_26);
+//        digitaBCMGpio.start();
+
+//        StepperMotorGpio motor = new StepperMotorGpio(RaspiBcmPin.GPIO_22, RaspiBcmPin.GPIO_23, RaspiBcmPin.GPIO_24, RaspiBcmPin.GPIO_25);
+//        motor.start();
         
-//        int i = 0;
-//        while(i == 0){
-//            fenetre.setHeureAffiche();
-//            Thread.sleep(1000);
-//        }
+////        int i = 0;
+////        while(i == 0){
+////            fenetre.setHeureAffiche();
+////            Thread.sleep(1000);
+////        }
 
         // test
     }
