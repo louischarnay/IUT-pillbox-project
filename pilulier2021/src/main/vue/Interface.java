49db5f0c58f83c4a7d139406f429de24cb65513d
Pablo Guinard <pablo.guinard@etu.univ-lyon1.fr>
2021-06-15 14:46:50 +0200

    bonjour

--- pilulier2021/src/main/vue/Interface.java
+++ pilulier2021/src/main/vue/Interface.java
@@ -7,10 +7,7 @@ package main.vue;
 
 import java.awt.Color;
 import java.awt.Font;
-import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -20,14 +17,11 @@ import java.awt.Image;
 import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.MouseEvent;
 import java.util.Date;
 import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
-import javax.swing.JComponent;
 import javax.swing.JTextArea;
 import javax.swing.border.Border;
-import main.vue.LedMarche;
 
 /**
  *
@@ -36,7 +30,7 @@ import main.vue.LedMarche;
 public class Interface extends JFrame implements ActionListener{
     protected JLabel heureAffiche=new JLabel();
     protected JTextArea infosMenu=new JTextArea();
-    protected JButton calendrier=new JButton(), informations=new JButton(), menuSU=new JButton(), panicButton=new JButton(), boutonAlerte=new JButton(), boutonMenuSU=new JButton();
+    protected JButton calendrier=new JButton(), informations=new JButton(), menuSU=new JButton(), panicButton=new JButton(), boutonAlerte=new JButton(), boutonMenuSU0=new JButton(), boutonMenuSU1=new JButton(), boutonMenuSU2=new JButton(), boutonRetour=new JButton();
     protected LedMarche ledMarche= new LedMarche();
     
     JPanel pano=new JPanel();
@@ -83,6 +77,10 @@ public class Interface extends JFrame implements ActionListener{
         menuSU.addActionListener(this);
         panicButton.addActionListener(this);
         boutonAlerte.addActionListener(this);
+        boutonRetour.addActionListener(this);
+        boutonMenuSU0.addActionListener(this);
+        boutonMenuSU1.addActionListener(this);
+        boutonMenuSU2.addActionListener(this);
        
     }
     
@@ -124,30 +122,58 @@ public class Interface extends JFrame implements ActionListener{
         calendrier.setBorderPainted(false);
         calendrier.setBackground(vertFond);
         
-        boutonAlerte.setText("Bonjour");
         boutonAlerte.setBackground(vertFond);
         bordure=BorderFactory.createLineBorder(Color.red);
         boutonAlerte.setBorder(bordure);
         boutonAlerte.setForeground(Color.red);
         boutonAlerte.setFont(new Font("Arial", Font.BOLD, 60));
         
-        //boutonMenuSU.setText
+        boutonMenuSU0.setBackground(vertFond);
+        bordure=BorderFactory.createLineBorder(Color.white);
+        boutonMenuSU0.setBorder(bordure);
+        boutonMenuSU0.setForeground(Color.white);
+        boutonMenuSU0.setFont(new Font("Arial", Font.BOLD, 50));
+        
+        boutonMenuSU1.setBackground(vertFond);
+        boutonMenuSU1.setBorder(bordure);
+        boutonMenuSU1.setForeground(Color.white);
+        boutonMenuSU1.setFont(new Font("Arial", Font.BOLD, 50));
+        
+        boutonMenuSU2.setBackground(vertFond);
+        boutonMenuSU2.setBorder(bordure);
+        boutonMenuSU2.setForeground(Color.white);
+        boutonMenuSU2.setFont(new Font("Arial", Font.BOLD, 50));
+        
+        boutonRetour.setText("Retour");
+        boutonRetour.setBackground(vertFond);
+        boutonRetour.setBorder(bordure);
+        boutonRetour.setForeground(Color.white);
+        boutonRetour.setFont(new Font("Arial", Font.BOLD, 30));
+        
         
         pano.setBackground(vertFond);
         //placement heure
-        heurePlacement(cont, pano);
+        heureAffiche(cont, pano);
         //placement prochain traitement + nb cases restantes
-        //prochainTraitPlacement(cont, pano);
-        boutonAlertePlacement(cont, pano, "Traitement pris");
-        //placement bouton calendrier
-        
+        infosMenuAffiche(cont, pano);
+        //placement bouton rouge
         //placement des boutons menu
-        boutonsMenuPlacement(cont, pano);
+        boutonsMenuAffiche(cont, pano);
         //placement led de marche
-        ledMarchePlacement(cont, pano);
+        ledMarcheAffiche(cont, pano);
+        //placement boutons menu SU
+        boutonMenuSUAffiche(cont, pano);
+        //placement bouton retour
+        boutonRetourAffiche(cont, pano);
+        
         
         this.setContentPane(pano);
         this.pack();
+        
+        
+        boutonMenuSUVisible(false);
+        boutonRetourVisible(false);
+        boutonAlerte.setVisible(false);
         
     }
     
@@ -161,55 +187,45 @@ public class Interface extends JFrame implements ActionListener{
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        if(e.getSource()==panicButton)
+        if(e.getSource()==panicButton){
+            infosMenuVisible(false);
+            boutonAlerteAffiche(cont, pano, "SAlut Adrien");
+            boutonAlerteVisible(true);
             System.out.println("panic button pressed");
-        else if(e.getSource()==menuSU)
-            System.out.println("menu SU pressed");
-        else if(e.getSource()==informations)
-            System.out.println("informations pressed");
-        else if(e.getSource()==calendrier)
-            System.out.println("calendrier pressed");
-        else if(e.getSource()==boutonAlerte)
+        }
+        
+        else if(e.getSource()==informations){
+            ledMarche.couleurLedChange(Color.green);
+            System.out.println("bouton ifnormations pressed");
+        }
+        else if(e.getSource()==calendrier){
+            ledMarche.couleurLedChange(Color.red);
+            System.out.println("bouton calendrier pressed");
+        }else if(e.getSource()==menuSU){
+            boutonAlerteVisible(false);
+            boutonsMenuVisible(false);
+            infosMenuVisible(false);
+            boutonRetourVisible(true);
+            boutonMenuSUVisible(true);
+        }
+        else if(e.getSource()==boutonAlerte){
             System.out.println("bouton alerte pressed");
-            boutonAlerte.setVisible(false);
-            prochainTraitPlacement(cont, pano);
+        }
+        else if(e.getSource()==boutonRetour){
+            boutonRetourVisible(false);
+            boutonMenuSUVisible(false);
+            boutonsMenuVisible(true);
+            infosMenuVisible(true);
+        }
+        else if(e.getSource()==boutonMenuSU0)
+            System.out.println("bouton menu SU0 pressed");
+        else if(e.getSource()==boutonMenuSU1)
+            System.out.println("bouton menu SU1 pressed");
+        else if(e.getSource()==boutonMenuSU2)
+            System.out.println("bouton menu SU2 pressed");
     }
     
-    
-    //fonctions de placement des éléments
-    public void heurePlacement(GridBagConstraints cont, JPanel pano){
-        cont.insets=new Insets(5, 5, 5, 5);
-        cont.fill=GridBagConstraints.BOTH;
-        cont.anchor=GridBagConstraints.NORTHWEST;
-        cont.gridwidth=3;
-        cont.gridx=0;
-        cont.gridy=0;
-        pano.add(heureAffiche, cont);
-        cont.gridwidth=1;
-    }
-    
-    public void ledMarchePlacement(GridBagConstraints cont, JPanel pano){
-        cont.gridy=0;
-        cont.gridx=3;
-        cont.fill=GridBagConstraints.NONE;
-        cont.anchor=GridBagConstraints.NORTHEAST;
-        pano.add(ledMarche, cont);
-    }
-    
-    public void prochainTraitPlacement(GridBagConstraints cont, JPanel pano){
-        cont.fill=GridBagConstraints.BOTH;
-        cont.gridheight=2;
-        cont.gridwidth=4;
-        cont.gridy=1;
-        cont.gridx=0;
-        cont.fill=GridBagConstraints.BOTH;
-        cont.insets=new Insets(50, 5, 50, 5);
-        pano.add(infosMenu, cont);
-        cont.gridheight=1;
-        cont.gridwidth=1;
-    }
-    
-    public void boutonsMenuPlacement(GridBagConstraints cont, JPanel pano){
+    public void boutonsMenuAffiche(GridBagConstraints cont, JPanel pano){
         cont.weightx=1;
         cont.insets=new Insets(5, 5, 5, 5);
         cont.gridy=3;
@@ -223,11 +239,11 @@ public class Interface extends JFrame implements ActionListener{
         pano.add(panicButton, cont);
     }
     
-    public void boutonAlertePlacement(GridBagConstraints cont, JPanel pano, String msg){
+    public void boutonAlerteAffiche(GridBagConstraints cont, JPanel pano, String msg){
         boutonAlerte.setText(msg);
         cont.fill=GridBagConstraints.BOTH;
         cont.anchor=GridBagConstraints.CENTER;
-        cont.insets=new Insets(70, 5, 70, 5);
+        cont.insets=new Insets(68, 5, 68, 5);
         cont.gridx=0;
         cont.gridheight=2;
         cont.gridwidth=4;
@@ -235,9 +251,101 @@ public class Interface extends JFrame implements ActionListener{
         pano.add(boutonAlerte, cont);
         cont.gridheight=1;
         cont.gridwidth=1;
+        cont.insets=new Insets(5, 5, 5, 5);
     }
     
-    public void menuMenuSUPlacement(GridBagConstraints cont, JPanel pano){
-        
+    public void boutonMenuSUAffiche(GridBagConstraints cont, JPanel pano){
+        cont.fill=GridBagConstraints.BOTH;
+        cont.gridx=0;
+        cont.gridy=1;
+        cont.gridwidth=4;
+        cont.insets=new Insets(45, 5, 5, 5);
+        boutonMenuSU0.setText("Informations");
+        pano.add(boutonMenuSU0, cont);
+        cont.insets=new Insets(5, 5, 5, 5);
+        cont.gridy=2;
+        boutonMenuSU1.setText("Remplissage");
+        pano.add(boutonMenuSU1, cont);
+        cont.gridy=3;
+        boutonMenuSU2.setText("Historique");
+        cont.insets=new Insets(5, 5, 45, 5);
+        pano.add(boutonMenuSU2, cont);
+        cont.insets=new Insets(5, 5, 5, 5);
+        cont.gridwidth=1;
+    }
+    
+    public void boutonRetourAffiche(GridBagConstraints cont, JPanel pano){
+        cont.fill=GridBagConstraints.BOTH;
+        cont.gridy=4;
+        cont.gridx=0;
+        cont.gridwidth=4;
+        cont.gridheight=1;
+        pano.add(boutonRetour, cont);
+    }
+    
+    public void heureAffiche(GridBagConstraints cont, JPanel pano){
+        cont.insets=new Insets(5, 5, 5, 5);
+        cont.fill=GridBagConstraints.BOTH;
+        cont.anchor=GridBagConstraints.NORTHWEST;
+        cont.gridwidth=3;
+        cont.gridx=0;
+        cont.gridy=0;
+        pano.add(heureAffiche, cont);
+        cont.gridwidth=1;
+    }
+    
+    public void ledMarcheAffiche(GridBagConstraints cont, JPanel pano){
+        cont.gridy=0;
+        cont.gridx=3;
+        cont.fill=GridBagConstraints.NONE;
+        cont.anchor=GridBagConstraints.NORTHEAST;
+        pano.add(ledMarche, cont);
+    }
+    
+    public void infosMenuAffiche(GridBagConstraints cont, JPanel pano){
+        cont.fill=GridBagConstraints.BOTH;
+        cont.gridheight=2;
+        cont.gridwidth=4;
+        cont.gridy=1;
+        cont.gridx=0;
+        cont.fill=GridBagConstraints.BOTH;
+        cont.insets=new Insets(52, 5, 50, 5);
+        pano.add(infosMenu, cont);
+        cont.gridheight=1;
+        cont.gridwidth=1;
+        cont.insets=new Insets(5, 5, 5, 5);
+    }
+    
+    
+    public void boutonsMenuVisible(boolean b){
+        calendrier.setVisible(b);
+        informations.setVisible(b);
+        menuSU.setVisible(b);
+        panicButton.setVisible(b);
+    }
+     
+    public void boutonAlerteVisible(boolean b){
+        boutonAlerte.setVisible(b);
+    }
+    public void boutonRetourVisible(boolean b){
+        boutonRetour.setVisible(b);
+    }
+    
+    public void infosMenuVisible(boolean b){
+        infosMenu.setVisible(b);
+    }
+    
+    public void ledMarcheVisible(boolean b){
+        ledMarche.setVisible(b);
+    }
+    
+    public void heureVisible(boolean b){
+        heureAffiche.setVisible(b);
+    }
+    
+    public void boutonMenuSUVisible(boolean b){
+        boutonMenuSU0.setVisible(b);
+        boutonMenuSU1.setVisible(b);
+        boutonMenuSU2.setVisible(b);
     }
 }
--- pilulier2021/src/main/vue/LedMarche.java
+++ pilulier2021/src/main/vue/LedMarche.java
@@ -16,16 +16,30 @@ import javax.swing.JComponent;
  * @author Pablo
  */
     public class LedMarche extends JComponent{
+        private Color couleurLed=Color.red;
+
+    public void setCouleurLed(Color couleurLed) {
+        this.couleurLed = couleurLed;
+    }
+        
+        
         @Override
         public Dimension getPreferredSize(){
             Dimension d=new Dimension(20, 20);
             return d;
         }
+        
+        public void couleurLedChange(Color couleur){
+            this.setCouleurLed(couleur);
+            System.out.println("fait");
+            repaint();
+                System.out.println("zebi");
+        }
         @Override
         protected void paintComponent(Graphics gra){
             gra.setColor(Color.white);
             gra.fillOval(0, 0, 20, 20);
-            gra.setColor(Color.red);
+            gra.setColor(couleurLed);
             gra.fillOval(3, 3, 14, 14);
     }
 
