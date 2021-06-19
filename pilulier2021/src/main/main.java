c7102d670bd641f8c2350d6888bf1fff96e7a0e4
CHARNAY LOUIS p2018172 <louis.charnay@etu.univ-lyon1.fr>
2021-06-19 19:06:43 +0200

    corrections

--- pilulier2021/src/main/main.java
+++ pilulier2021/src/main/main.java
@@ -34,6 +34,9 @@ public class main {
      * @param args the command line arguments
      */
     public static void main(String[] args) throws InterruptedException, FileNotFoundException, UnsupportedEncodingException, IOException {;
+//        String mail = "sendemail -f mail.charnay@orange.fr -t louis.charnay@orange.fr -u subject -m \"message\" -s smtp.orange.fr:587 -o starttls=yes -xu mail.charnay@orange.fr -xp Maison_01000";
+//        Runtime.getRuntime().exec(mail);
+        
         ArrayList<Case> calendrier=new ArrayList<>();
         ArrayList<Referent> referents=new ArrayList<>();
         Referent patient=new Referent("...","...","Patient","...","...","...");
@@ -53,8 +56,8 @@ public class main {
         calendrier.add(new Case(7,new Date()));
           
         
-        Moteur motor = null; //si moteur non raccordé à la raspberry
-//        Moteur motor = new Moteur(0, RaspiBcmPin.GPIO_22, RaspiBcmPin.GPIO_23, RaspiBcmPin.GPIO_24, RaspiBcmPin.GPIO_25); //si moteur raccordé à la raspberry
+//        Moteur motor = null; //si moteur non raccordé à la raspberry
+        Moteur motor = new Moteur(0, RaspiBcmPin.GPIO_22, RaspiBcmPin.GPIO_23, RaspiBcmPin.GPIO_24, RaspiBcmPin.GPIO_25); //si moteur raccordé à la raspberry
         HautParleur buzzer = null; //si hp non raccordé à la raspberry
 //        HautParleur buzzer = new HautParleur(0, RaspiBcmPin.GPIO_26); //si hp raccordé à la raspberry
 
